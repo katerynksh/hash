147292962600f9eef7b0f499111cd82c4e814608
Sherepera Kateryna <katerynasherepera@gmail.com>
2026-03-10 22:40:02 +0200

    add

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# hash
+# Quick start:
+1. Install dependencies: `npm install`
+2. Run the script with a password argument: `node hash.js your_password`
+
+
+# About:
+### This code demonstrates how to use bcrypt for hashing passwords in Node.js. It checks if a hash already exists in a file, and if not, it creates one and saves it. If a hash exists, it compares the input password with the stored hash to authenticate the user.
---
+++ hash.js
@@ -0,0 +1,41 @@
+import bcrypt from 'bcrypt';
+import { hash } from 'node:crypto';
+import { createReadStream, read } from 'node:fs';
+import { readFileSync, writeFileSync, existsSync } from 'node:fs';
+
+const saltRounds = 10;
+const fileName = 'passwords.txt';
+const inputPassword = process.argv[2];
+
+if (!inputPassword) {
+    console.error("Please enter a password: node hash.js <your_password>");
+    process.exit(1);
+}
+let storedHash = '';
+if (existsSync(fileName)) {
+    storedHash = readFileSync(fileName, 'utf8').trim();
+}
+
+bcrypt.compare(
+    inputPassword,
+    '$2b$10$gXKuVtPpSqc1N.qdLWejdOtFQWfS5ivQSesB2iX/V2CgSgoh5Vmzq',
+).then ((data) => {
+    console.log(data);        
+}).catch((err) => {
+    console.error(err);
+});
+if (!storedHash) {
+    // Якщо хешу немає — створюємо його і зберігаємо у файл
+    const hash = await bcrypt.hash(inputPassword, saltRounds);
+    writeFileSync(fileName, hash);
+    console.log(`Hash saved to ${fileName}`);
+} else {
+    // Якщо хеш є — порівнюємо його з введеним паролем
+    const isMatch = await bcrypt.compare(inputPassword, storedHash);
+    
+    if (isMatch) {
+        console.log("Authenticated successfully");
+    } else {
+        console.log("Wrong password");
+    }
+}
