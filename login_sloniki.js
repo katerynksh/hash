a9fec70f073b9e43b4a9de0944924e64db6e8a06
Sherepera Kateryna <katerynasherepera@gmail.com>
2026-03-14 12:58:13 +0200

    added deleting with password and better fotmatting

--- login_sloniki.js
+++ login_sloniki.js
@@ -1,11 +1,10 @@
 import bcrypt from 'bcrypt';
-// import { hash } from 'node:crypto';
-// import { createReadStream, read } from 'node:fs';
-import { readFileSync, writeFileSync, existsSync } from 'node:fs';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import pg from 'pg';
 
-dotenv.config()
+dotenv.config( )
+
+const SALT_ROUNDS = 10;
 
 const { Pool } = pg;
 const pool = new Pool({
@@ -16,7 +15,8 @@ const pool = new Pool({
 });
 
 const initializeDatabase = async () => {
-   console.log('🔄️ Initializing sloniki database...');
+   console.log('-----------------------------------------------');
+   console.log('Initializing sloniki database...');
 
    const createTableQuery = `
     CREATE TABLE IF NOT EXISTS sloniki (
@@ -24,21 +24,31 @@ const initializeDatabase = async () => {
     username TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,           
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-`;
+   );
+      `;
    try {
       await pool.query(createTableQuery);
-    //   console.log('✅  The sloniki table is ready to go.');
    } catch (error) {
-      console.error('❗ Error initializing database:', error.message);
-      console.error('Full error:', error);
+      console.error('!! Error initializing database');
+      // console.error('Full error:', error);
       throw error;
    }
 };
-const saltRounds = 10;
+
+
+async function getAllSloniki() {
+   const res = await pool.query('SELECT * FROM sloniki ORDER BY id ASC'); //ASC - по зростанню, DESC - спадання
+   console.log('✨ List of all sloniki:');
+   console.table(res.rows.map(slonik => ({
+      id: slonik.id,
+      username: slonik.username,
+      created_at: slonik.created_at
+   })));
+}
+
 async function registerSlonik(username, password) {
    try {
-      const hash = await bcrypt.hash(password, saltRounds);
+      const hash = await bcrypt.hash(password, SALT_ROUNDS);
       const query = `
       INSERT INTO sloniki (
             username, password_hash
@@ -47,118 +57,131 @@ async function registerSlonik(username, password) {
         RETURNING *`;
    const res = await pool.query(query, [username, hash]);
     
-    console.log('✅ Slonik registered successfully: @',res.rows[0].username);
-    console.log('✅ Password hash stored securely.');
-  } catch (err) {
-    console.error('❗ Error registering slonik:', err.message);
-  }
+    console.log(`✓ Slonik registered successfully: @${res.rows[0].username}`);
+    console.log('✓ Password hash stored securely.');
+   } catch (err) {
+      console.error(`!! Error registering slonik: this slonik: @${username} is already exist`);
+   }
 }
 
 async function loginSlonik(username, password) {
    try {
       const res = await pool.query('SELECT * FROM sloniki WHERE username = $1', [username]);
       if (res.rows.length === 0) {
-         console.log('❌ No slonik found with that username.');
+         console.log(`??? No slonik found with that username @${username}.`);
          return;
       }
       const slonik = res.rows[0];
       const isMatch = await bcrypt.compare(password, slonik.password_hash);
       if (isMatch) {
-         console.log('✅ Login successful.');
+         console.log('✓ Login successful.');
       } else {
-         console.log('❌ Invalid password.');
+         console.log('??? Invalid password.');
       }
    } catch (err) {
-      console.error('❗ Error during login:', err.message);
+      console.error('!! Error during login');
    }
 }
 
 
-async function deleteSlonik(id) {
-   await pool.query('DELETE FROM sloniki WHERE id = $1', [id]);
-   console.log(`✅  The slonik with ID ${id} has been removed from the database..`);
+async function deleteSlonik(username, password) {
+   try {
+      const res = await pool.query('SELECT * FROM sloniki WHERE username = $1', [username]);
+      if (res.rows.length === 0) {
+         console.log('??? No slonik found with that username.');
+         return;
+      }
+      const slonik = res.rows[0];
+      const isMatch = await bcrypt.compare(password, slonik.password_hash);
+      if (isMatch) {
+         await pool.query('DELETE FROM sloniki WHERE username = $1', [username]);
+         console.log(`✓  The slonik @${username} has been removed from the database..`);
+      } else {
+         console.log('??? Invalid password.');
+      }
+   } catch (err) {
+      console.error('!! Error during deletion');
+   }
 }
-
-async function getAllSloniki() {
-   const res = await pool.query('SELECT * FROM sloniki ORDER BY id ASC');
-   console.log('✨ List of all sloniki:');
-   console.table(res.rows);
-}
-const run = async () => {
-   await initializeDatabase();
-}
-const username = process.argv[3];
-const password = process.argv[4];
-const newPassword = process.argv[5];
 
 async function updateSlonikPassword(username, password, newPassword) {
    try {
       const res = await pool.query('SELECT * FROM sloniki WHERE username = $1', [username]);
       if (res.rows.length === 0) {
-         console.log('❌ No slonik found with that username.');
+         console.log('??? No slonik found with that username.');
          return;
       }
-   
+
       const slonik = res.rows[0];
       const isMatch = await bcrypt.compare(password, slonik.password_hash);
       if (isMatch) {
-      try {
-         const hash = await bcrypt.hash(newPassword, saltRounds);
-         await pool.query('UPDATE sloniki SET password_hash = $1 WHERE username = $2', [hash, username]);
-         console.log(`✅ Password updated for  @${username}`);
-      } catch (err) {
-      console.error('❗ Error updating slonik password:', err.message);
-      }
+         try {
+            const hash = await bcrypt.hash(newPassword, SALT_ROUNDS);
+            await pool.query('UPDATE sloniki SET password_hash = $1 WHERE username = $2', [hash, username]);
+            console.log(`✓ Password updated for  @${username}`);
+         } catch (err) {
+            console.error('!! Error updating slonik password',);
+         }
       } else {
-         console.log('❌ Invalid password.');
+         console.log('??? Invalid password.');
       }
    } catch (err) {
-      console.error('❗ Error during password update:', err.message);
+      console.error('!! Error during password update');
    }
 }
 
+await initializeDatabase();
 
-switch (process.argv[2]) {
+const command = process.argv[2];
+const username = process.argv[3];
+const password = process.argv[4];
+const newPassword = process.argv[5];
+
+switch (command) {
    case 'list':
       await getAllSloniki();
       break;
    case 'register':
       if (!username || !password) {
-         console.error('❗ Please enter both username and password: node <fileName>.js register <username> <password>');
+         console.error('!! Please enter both username and password: node <fileName>.js register <username> <password>');
          process.exit(1);
       }
       await registerSlonik(username, password);
       break;
-    case 'delete':
-        await deleteSlonik (process.argv[3]);
-        break;
-    case 'update-password':
-      if (!username || !password || !newPassword) {
-         console.error('❗ Please enter username, current password and new password: node <fileName>.js update-password <username> <current_password> <new_password>');
-         process.exit(1);
-      }
-      await updateSlonikPassword(process.argv[3], process.argv[4], process.argv[5]);
-      break;
    case 'login':
       if (!username || !password) {
-         console.error('❗ Please enter both username and password: node <fileName>.js login <username> <password>');
+         console.error('!! Please enter both username and password: node <fileName>.js login <username> <password>');
          process.exit(1);
       }
       await loginSlonik(username, password);
       break;
-
-      case 'help':
+   case 'delete':
+      await deleteSlonik (username, password);
+      if (!username || !password) {
+      console.error('!! Please enter username and current password: node <fileName>.js delete <username> <current_password>');
+      process.exit(1);
+   }
+      break;
+   case 'updatePassword':
+      if (!username || !password || !newPassword) {
+         console.error('!! Please enter username, current password and new password: node <fileName>.js update-password <username> <current_password> <new_password>');
+         process.exit(1);
+      }
+      await updateSlonikPassword(username, password, newPassword);
+      break;
+   case 'help':
       console.log('————————————————————————————————————————————');
       console.log('Available commands:');
-      console.log('1️⃣  list - Display all elephants');
-      console.log('2️⃣  register + <username> <password> - Register a new slonik');
-      console.log('3️⃣  login + <username> <password> - Login as a slonik');
-      console.log('4️⃣  update-password + <username> <current_password> <new_password> - Update a slonik password');
-      console.log('5️⃣  delete + <id> - Delete a slonik by ID');
-      console.log('6️⃣  help - Display available commands');
+      console.log('1️  list - Display all sloniki');
+      console.log('2️  register + <username> <password> - Register a new slonik');
+      console.log('3️  login + <username> <password> - Login as a slonik');
+      console.log('4️  update-password + <username> <current_password> <new_password> - Update a slonik password');
+      console.log('5️  delete + <id> - Delete a slonik by ID');
+      console.log('6️  help - Display available commands');
       console.log('—————————————————————————————————————————————')
       break;
    default:
-      console.log('❗ Unknown command. Use "help" to see all available commands.');
+      console.log('!! Unknown command. Use "help" to see all available commands.');
       break;
 }
+console.log('-----------------------------------------------');
